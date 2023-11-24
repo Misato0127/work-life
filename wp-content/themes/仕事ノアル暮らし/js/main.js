f456513037220ab249907d631763b4941244ca3d
Misato Shiojima <kuroinobori@gmail.com>
2023-11-24 13:55:38 +0900

    phpを追加

---
+++ wp-content/themes/仕事ノアル暮らし/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slick-area').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3
+});
+
+$('.main-s').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+});
